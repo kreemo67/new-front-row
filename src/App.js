300ab4a414dea6f4db9a0ab9c7397f1762fe683c
Kareem Abdo <kreemo67@gmail.com>
2022-02-20 20:26:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from V2 my guy! Only the beginning and only up from here.</h1>
       </header>
     </div>
   );
